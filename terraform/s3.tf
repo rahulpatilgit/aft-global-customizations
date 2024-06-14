1f0e1eb89e5f65771ff7fb8ce0b221b3f461f2f1
Rahul <115698783+architectrahul@users.noreply.github.com>
2024-06-14 10:10:27 +0530

    Remove s3 bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,6 @@
-data "aws_caller_identity" "current" {}
+# data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "sandbox_bucket_new" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}-new"
-  acl    = "private"
-}
+# resource "aws_s3_bucket" "sandbox_bucket_new" {
+#   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}-new"
+#   acl    = "private"
+# }
